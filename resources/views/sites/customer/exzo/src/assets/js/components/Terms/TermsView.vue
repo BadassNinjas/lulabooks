cdb3f5c7a691ff51506b17461b39182d7767c458
Luxolo Mbaleni <lux589>
2017-03-14 13:58:56 +0200

    update terms and conditions

--- resources/views/sites/customer/exzo/src/assets/js/components/Terms/TermsView.vue
+++ resources/views/sites/customer/exzo/src/assets/js/components/Terms/TermsView.vue
@@ -20,7 +20,7 @@
         <div class="h5"><u>Electronic Communications</u></div><br>
         <p class="simple-article size-4">By using this Shop and/or communicating with the Provider by electronic means, the User consents and acknowledges that any and all agreements, notices, disclosures, or any other communication satisfies all legal requirements, including but not limited to the requirement that such communications should be in writing.</p>
         <br>
-        <div class="h5">E-Commerce and Privacy</div><br>
+        <div class="h5"><u>E-Commerce and Privacy</u></div><br>
         <p class="simple-article size-4">The Shop www.lulabooks.mobi sells products and/or services online.  The use of any product and/or service bought from this Shop is at the purchaser’s risk. The User indemnifies and holds the Provider harmless against any loss, injury or damages which may be sustained as a result of using the products and or services sold on the Shop.</p>
         <br>
         <p class="simple-article size-4">The Provider is not responsible for security breaches occurring on the User’s browser that may result due to the lack of adequate virus protection software, malware or spyware that the User may inadvertently have installed on his/her device, or from any reason whatsoever.</p>
@@ -29,7 +29,7 @@
         <br>
         <p class="simple-article size-4">The Provider will supply physical goods to a delivery company in good order. The Provider will however not be held liable for the condition of physical goods arriving at the User’s chosen delivery address.</p>
         <br>
-        <div class="h5">Refund and Return Policy</div><br>
+        <div class="h5"><u>Refund and Return Policy</u></div><br>
         <p class="simple-article size-4">The provision of goods and services is subject to functional availability. In cases of unavailability, the provider will refund the client in full within 30 days. </p><br>
         <p class="simple-article size-4">In terms of the Consumer Protection Act (Section 20), the User may inspect the goods and services on delivery to meet the type or quality that the User may reasonably expect in terms of the information provided by the Provider on the Shop or by other means.</p><br>
         <p class="simple-article size-4">Inspection means reasonable viewing of the goods and services, and not full use thereof. Should the goods consist of an online course or courses, then the User agrees that reasonable viewing consists of less than 4 minutes or 25% of the course and not more.</p><br>
@@ -39,18 +39,26 @@
         <p class="simple-article size-4">In terms of Section 55 of the CPA, if the User informs the Provider in writing that the goods and services are required for a particular purpose, and the Provider advises the User that this purpose will be met, and the User can then show objectively that the goods do not meet the purpose, then the User can return the goods up to 10 days after purchase, receive a full refund, and cancel without penalty.  The Provider will then be liable for the cost of returning the goods.</p><br>
         <p class="simple-article size-4">The Provider reserves the right to cancel an order for which payment has already been received.  This may occur if stock is insufficient or the quality of goods ordered does not meet the Provider’s standards.  Should the Provider exercise this right, the User will receive a full refund with no deductions.</p><br>
         <p class="simple-article size-4">Any complaints regarding the standard and quality of the goods and services, or requests for refunds on goods and services, bought by Users through the Shop should be directed in writing to the Marketing Manager [info@lulabooks.mobi]. </p><br>
-        <div class="h5">Updating of these Terms and Conditions</div><br>
+        <div class="h5"><u>Updating of these Terms and Conditions</u></div><br>
         <p class="simple-article size-4">The Provider reserves the rights to change, modify, add or remove from portions or the whole of these Terms and Conditions from time to time. Changes to these Terms and Conditions will become effective upon such changes being posted to this Shop. It is the User’s obligation to check these Terms and Conditions at the time of checkout. The User’s continued use of this e-commerce platform will be considered the User’s acceptance to abide by and be bound by these Terms and Conditions, including all changes or updates.</p><br>
-        <div class="h5">Copyright and Intellectual Property Rights</div><br>
+        <div class="h5"><u>Copyright and Intellectual Property Rights</u></div><br>
         <p class="simple-article size-4">Provider provides certain information at the Shop. Content currently or anticipated to be displayed at this Shop is provided by Provider, its affiliates and/or subsidiary, or any other third party owners of such content, and includes but is not limited to Literary Works, Musical Works, Artistic Works, Sound Recordings, Cinematograph Films, Sound and Television Broadcasts, Program-Carrying Signals, Published Editions, online courses, webinars, training material and Computer Programs (“the Content”). All such proprietary works, and the compilation of the proprietary works, are copyright the Provider, its affiliates or subsidiary, or any other third party owner of such rights (“the Owners”), and are protected by South African and international copyright laws. The Provider reserves the right to make any changes to the Shop, the Content, or to products and/or services offered through the Shop at any time and without notice. All rights in and to the Content is reserved and retained by the Owners. Except as specified in these Terms and Conditions, the User is not granted a license or any other right including without limitation under Copyright, Trademark, Patent or other Intellectual Property Rights in or to the Content.</p><br>
-        <div class="h5">Limitation of liability</div><br>
+        <div class="h5"><u>Limitation of liability</u></div><br>
         <p class="simple-article size-4">The Shop and all Content on the Shop, including any current or future offer of products or services, are provided on an “as is” basis, and may include inaccuracies or typographical errors. The Provider and/or Owners make no warranty or representation as to the availability, accuracy or completeness of the Content. Neither Provider nor the Owners and any holding company, affiliate or subsidiary of Provider, shall be held responsible for any direct or indirect special, consequential or other damage of any kind whatsoever suffered or incurred, related to the use of, or the inability to access and/or use the Content or the Shop and/or any functionality thereof, or of any linked Shop, even if Provider is expressly advised thereof.</p><br>
-        <div class="h5">Privacy: Casual surfing</div><br>
+        <div class="h5"><u>Privacy: Casual surfing</u></div><br>
         <p class="simple-article size-4">The User may visit the Shop without providing any personal information. The Shop servers will in such instances collect the IP address of the User computer, but not the email address or any other distinguishing information. This information is aggregated to measure the number of visits, average time spent at the Shop, pages viewed, etc. Provider uses this information to determine use of the Shop, and to improve Content thereon. Provider assumes no obligation to protect this information, and may copy, distribute or otherwise use such information without limitation.</p><br>
-        <div class="h5">Choice of Law</div><br>
+        <div class="h5"><u>Choice of Law</u></div><br>
         <p class="simple-article size-4">This Shop is controlled, operated and administered by Provider from its offices within the Republic of South Africa. Access to the Shop from territories or countries where the Content or purchase of the products sold on the Shop is illegal is prohibited. The User may not use this Shop in violation of South African export laws and regulations. If the User accesses this Shop from locations outside of South Africa, that User is responsible for compliance with all local laws. These Terms and Conditions shall be governed by the laws of the Republic of South Africa, and the User consents to the jurisdiction of the Western Cape High Court in the event of any dispute. If any of the provisions of these Terms and Conditions are found by a court of competent jurisdiction to be invalid or unenforceable, that provision shall be enforced to the maximum extent permissible so as to give effect to the intent of these Terms and Conditions, and the remainder of these Terms and Conditions shall continue in full force and effect. These Terms and Conditions constitute the entire agreement between the Provider and the User with regard to the use of the Content and this Shop.</p><br>
       </div>
 
+    </div>
+    <div class="empty-space col-xs-b50"></div>
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <div class="title-underline left"><span></span></div>
+            <div class="h2">End of Terms and Conditions</div>
+            <div class="title-underline left"><span></span></div>
+        </div>
     </div>
     <div class="empty-space col-xs-b50"></div>
     <div class="empty-space col-xs-b50"></div>
